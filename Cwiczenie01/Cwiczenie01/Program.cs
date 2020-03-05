a1f74521fb6da6459b538eff4c82d9fb0b171f59
Michał Terlecki <s18801@pjwstk.edu.pl>
2020-03-05 13:08:40 +0100

    Initial commit

--- Cwiczenie01/Cwiczenie01/Program.cs
+++ Cwiczenie01/Cwiczenie01/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cwiczenie01
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //if (args.Length == 0) return;
+
+            var client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+            //Task<T>
+            //ThreadPool()
+
+            if (!result.IsSuccessStatusCode) return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+", RegexOptions.IgnoreCase);
+            var matches = regex.Matches(html);
+            foreach(var m in matches)
+            {
+                Console.WriteLine(m);
+            }
+
+
+            Console.WriteLine("Koniec!");
         }
     }
 }
