23115ac9d16aa63a34efa475dfa38ceab08671bb
Michał Terlecki <s18801@pjwstk.edu.pl>
2020-03-05 13:32:37 +0100

    Initial commit

--- Cwiczenie01/Cwiczenie01/Program.cs
+++ Cwiczenie01/Cwiczenie01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,21 +13,41 @@ namespace Cwiczenie01
         {
             //if (args.Length == 0) return;
 
-            var client = new HttpClient();
-            var result = await client.GetAsync("https://www.pja.edu.pl");
-            //Task<T>
-            //ThreadPool()
+            string url = args.Length > 0 ? args[0] : "https://www.pja.edu.pl";
 
-            if (!result.IsSuccessStatusCode) return;
-
-            string html = await result.Content.ReadAsStringAsync();
-            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+", RegexOptions.IgnoreCase);
-            var matches = regex.Matches(html);
-            foreach(var m in matches)
+            try
             {
-                Console.WriteLine(m);
-            }
+                var client = new HttpClient();
+                var result = await client.GetAsync(url);
+                //Task<T>
+                //ThreadPool()
 
+                if (!result.IsSuccessStatusCode) return;
+
+                //Kolekcje
+                var zbiory = new HashSet<string>();
+                var list = new List<string>();
+                var slownik = new Dictionary<string, int>();
+
+
+                var znalezione = from e in list
+                                 where e.StartsWith("A")
+                                 select e;
+                var znalezione2 = list.Where(s => s.StartsWith("A"));
+
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(html);
+                foreach (var m in matches)
+                {
+                    Console.WriteLine(m);
+                }
+
+            }catch(Exception exc)
+            {
+                //string s = string.Format("Wystapil blad {0}", exc.toString());
+                Console.WriteLine($"Wystapil blad {exc.ToString()}");
+            }
 
             Console.WriteLine("Koniec!");
         }
