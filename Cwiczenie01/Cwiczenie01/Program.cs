4c5cd8c86224c66dcec7a00f76c9117db56f5e36
Michał Terlecki <s18801@pjwstk.edu.pl>
2020-03-05 12:41:15 +0100

    Initial commit

---
+++ Cwiczenie01/Cwiczenie01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cwiczenie01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
